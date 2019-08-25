16e36c1adb7908a068d1d4d94f198528ba0997c7
Putrevu, Phani <phani.putrevu@precor.com>
2019-08-25 15:01:57 -0700

    Phani's changes

---
+++ examples/webapp-library/parent/build/precache-manifest.4e489b73596e9f15dba812c4818495f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16deeaa5bfb8553d97cd38399bf99c07",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3429f1f46d1c25b5c74c",
+    "url": "./static/css/main.a532201e.chunk.css"
+  },
+  {
+    "revision": "9c30f59c9a434f79a9a5",
+    "url": "./static/js/2.63f60684.chunk.js"
+  },
+  {
+    "revision": "3429f1f46d1c25b5c74c",
+    "url": "./static/js/main.aeaf3314.chunk.js"
+  },
+  {
+    "revision": "574894ead2dabc320a02",
+    "url": "./static/js/runtime~main.962b2c08.js"
+  }
+]);
